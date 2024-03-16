e59a917906dcc890196c854cd265cbafa07d7cc1
Sandesh Bhandari <36527732+sandesh0418@users.noreply.github.com>
2024-03-16 15:41:17 -0400

    test

---
+++ src/main/java/com/hms/healthmgmtsvc/HealthMgmtSvcApplication.java
@@ -0,0 +1,14 @@
+package com.hms.healthmgmtsvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class HealthMgmtSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthMgmtSvcApplication.class, args);
+	}
+
+}
